c21bb24dbf25e72eb09526f910133a23c1bc1113
Jia Cao <caojia@hulu.com>
2011-10-29 16:22:45 +0800

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeiboDiary::Application.config.secret_token = '83c6cd73efe16aeb3d6b3b78fa4ab3ef38eac58dc94390433865da79c80d367d4817d6de9be69cabeb033ae59144652f1f52bf49e24fdcc8331b59b0b2265a0d'
