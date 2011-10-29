d3eaab16d3e8922b72e17e3f61239cffe68c1060
Jia Cao <caojia@hulu.com>
2011-10-29 20:48:09 +0800

    fix before_filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def authorize
+    if !check_authorize
+      redirect_to "/users/welcome"
+    end
+  end
+
+  def check_authorize
+    result = true
     begin
       @oauth = Weibo::OAuth.new(Weibo::Config.api_key, Weibo::Config.api_secret)
       if !session[:atoken].blank? && !session[:asecret].blank?
         @oauth.authorize_from_access(session[:atoken], session[:asecret])
       else
-        redirect_to "/users/welcome"
+        result = false
       end
     rescue
-      redirect_to "/users/welcome"
+      result = false
     end
+    return result
   end
+
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,8 @@
 class TweetsController < ApplicationController
-
+  before_filter :authorize
   # Landing Page
   def home
-    if authorize 
+    if check_authorize 
       redirect_to "/tweets/index"
     else
       redirect_to "/users/welcome"
@@ -12,8 +12,7 @@ class TweetsController < ApplicationController
   # GET /tweets
   # GET /tweets.xml
   def index
-    authorize
-    @tweets = Weibo::Base.new(@oauth).user_timeline
+    @tweets = Weibo::Base.new(@oauth).user_timeline({:count => 100})
 
     respond_to do |format|
       format.html { render "tweets/index" }
@@ -22,7 +21,6 @@ class TweetsController < ApplicationController
   end
 
   def friends_timeline
-    authorize
     @tweets = Weibo::Base.new(@oauth).friends_timeline
     respond_to do |format|
       format.html { render "/tweets/friends_timeline" }
@@ -33,6 +31,19 @@ class TweetsController < ApplicationController
   # GET /tweets/1
   # GET /tweets/1.xml
   def show
-
   end
+
+  protected
+    def group_tweets tweets
+      tweets.sort! {|t1, t2| t1.created_at <=> t2.created_at}
+      grouped = []
+      last_time = nil
+      group = []
+      tweets.each do |tweet|
+        t = tweet.created_at.to_i / 3600 # get hour
+        if t != last_time
+        end
+      end
+    end
+
 end
