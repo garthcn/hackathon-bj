01f78a8a5a3e500aa0ac9173186a39e725e1446f
Garth <garthcn@gmail.com>
2011-10-29 17:22:02 +0800

    add layout

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,7 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 WeiboDiary::Application.config.secret_token = '8f0bdfb7b717200098bef533bfd00339bf7be0f02b0a0a1c8ea6b8d8b0526988edadd547f15f1b425ec17a88635730e2202cfbd64ef1deac6f7a84cc6ce95d94'
+Weibo::Config.api_key = '779594116'  
+Weibo::Config.api_secret = 'cae09b103fccc2ba40eac829a2d9bda0'
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ WeiboDiary::Application.routes.draw do
   resources :tweets
 
   resources :users
+  root :to => "tweets#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +53,6 @@ WeiboDiary::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
